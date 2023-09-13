98ce31663b0034f7ed7b8806d79879d4830ea313
FabriBoscatto <126684761+FabriBoscatto@users.noreply.github.com>
2023-09-13 18:23:38 -0300

    subida del proyecto

---
+++ js/index.js
@@ -0,0 +1,46 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Controles siguienta/anterior
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// controles de imagen en miniatura
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+    /* controles de acordeon */
+    this.classList.toggle("activo");
+
+    /* Alternar entre ocultar y mostrar el panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+});
+}
